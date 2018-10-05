7380ed72650f455ec1f564e20ad4d5c949e15876
srujan <srujanprophet@gmail.com>
2018-10-05 21:44:04 +0530

    Add Frontend for login and registration

--- food-delivery/src/App.js
+++ food-delivery/src/App.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
+import SignIn from './components/SignIn/SignIn';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Register from './components/Register/Register';
 import './App.css';
+import 'tachyons';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1> Food Delivery App </h1>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			route: 'signin',
+			isSignedIn: false,
+		}
+	}
+
+	onRouteChange = (route) => {
+		if(route === 'signout') {
+	      this.setState({isSignedIn:false})
+	    } else if (route === 'home') {
+	      this.setState({isSignedIn:true})
+	    }
+	    this.setState({route:route});
+	}
+
+	render() {
+		return (
+		  <div className="App">
+		  	<div>
+		  	<Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+		  	</div>
+		  	{ ( this.state.route === 'home' ) ?
+		  		<Logo /> :
+		  		(this.state.route === 'signin') ?
+		    	<SignIn onRouteChange={this.onRouteChange} /> :
+		    	<Register onRouteChange={this.onRouteChange} />
+			}
+		  </div>
+		);
+	}
 }
 
 export default App;
